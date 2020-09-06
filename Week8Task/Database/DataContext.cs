1f8f0c53a60eca7fef30c1d2a7859620c0acc0de
Netochukwu Anyankah <nanyankah@gmail.com>
2020-09-06 16:37:51 +0100

    Add project files.

---
+++ Week8Task/Database/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Week8Task.Models;
+
+namespace Week8Task.Database
+{
+   public class DataContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+         
+
+    }
+}
